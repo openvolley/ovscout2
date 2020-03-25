471e466ce48775a5f9076c494b0abdac386fff79
Ben Raymond <ben@theraymonds.org>
2020-03-25 16:29:06 +1100

    fix tests for interpreter mods

--- tests/testthat/test_code_interpreter.R
+++ tests/testthat/test_code_interpreter.R
@@ -12,21 +12,18 @@ test_that("code interpretation works correctly", {
     check_code(ov_code_interpret("*2AT#45H2"), "*02AT#~~~45~H2")
     check_code(ov_code_interpret("*2AT#452"), "*02AT#~~~45~H2")
     check_code(ov_code_interpret("*2AT#45P2"), "*02AT#~~~45~P2")
-    check_code(ov_code_interpret("*5X5!"), "*05AT!X5F4~~H")
-    check_code(ov_code_interpret("5X5!"), "*05AT!X5F4~~H")
-    check_code(ov_code_interpret("5X5"), "*05AT+X5F4~~H") ## default evaluation
-    check_code(ov_code_interpret("5X55"), "*05AT+X5F45~H") 
-    check_code(ov_code_interpret("*5X5+5H2"), "*05AT+X5F45~H2")
-    check_code(ov_code_interpret("*5X55H2"), "*05AT+X5F45~H2")
+    check_code(ov_code_interpret("*5X5!"), "*05AT!X5~4~~H")
+    check_code(ov_code_interpret("5X5!"), "*05AT!X5~4~~H")
+    check_code(ov_code_interpret("5X5"), "*05AT+X5~4~~H") ## default evaluation
+    check_code(ov_code_interpret("5X55"), "*05AT+X5~45~H") 
+    check_code(ov_code_interpret("*5X5+5H2"), "*05AT+X5~45~H2")
+    check_code(ov_code_interpret("*5X55H2"), "*05AT+X5~45~H2")
     check_code(ov_code_interpret("a12SM.2#78"), c("a12SM-~~~78", "*02RM#~~~78"))
-    check_code(ov_code_interpret("*2X5.15=5H4"), c("*02AT#X5F45~H4", "a15BT=~~~~5~~4"))
+    check_code(ov_code_interpret("*2X5.15=5H4"), c("*02AT#X5~45~H4", "a15BT=~~~~5~~4"))
     check_code(ov_code_interpret("2AT#452"), "*02AT#~~~45~H2") 
     check_code(ov_code_interpret("2AT#45t1"), "*02AT#~~~45~T1")
-    check_code(ov_code_interpret("2X5#.15"), c("*02AT#X5F4", "a15BT=")) ## block error is the default on attack kill
-    check_code(ov_code_interpret("2X5#.15D"), c("*02AT#X5F4", "a15DT=")) ## but we can specify e.g. dig error.
+    check_code(ov_code_interpret("2X5#.15"), c("*02AT#X5~4", "a15BT=")) ## block error is the default on attack kill
+    check_code(ov_code_interpret("2X5#.15D"), c("*02AT#X5~4", "a15DT=")) ## but we can specify e.g. dig error.
     check_code(ov_code_interpret("2SQ1.5=4"), c("*02SQ#~~~14", "a05RQ=~~~14"))
-    check_code(ov_code_interpret("2X5#5S"), "*02AT#X5F45~H~S") ## Include special syntax
+    check_code(ov_code_interpret("2X5#5S"), "*02AT#X5~45~H~S") ## Include special syntax
 })
-
-## test cases to think about:
-## should "2X5#5S" be "*02AT#X5F45" - i.e. should the trailing S be ignored? it's not valid for attacks, at least in the default tables
