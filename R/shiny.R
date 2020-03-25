256e571203b1a8ae165667d3193476bad93a24e1
Ben Raymond <ben@theraymonds.org>
2020-03-26 07:45:55 +1100

    pass args in ... of ov_shiny_video_sync to its server/ui funcs

--- R/shiny.R
+++ R/shiny.R
@@ -3,7 +3,7 @@
 #' @param dvw string or datavolley: either the path to a dvw file (which will be read by \code{\link[datavolley]{read_dv}}) or a datavolley object as returned by that function. Passing the file name (not the datavolley object) is required if any extra arguments are passed via \code{...}
 #' @param video_file string: optionally, the path to the video file. If not supplied (or \code{NULL}) the video file specified in the dvw file will be used
 #' @param launch_browser logical: if \code{TRUE}, launch the app in the system's default web browser (passed to \code{\link[shiny]{runApp}}'s \code{launch.browser} parameter)
-#' @param ... : extra parameters passed to \code{\link[datavolley]{read_dv}} (if \code{dvw} is a provided as a string)
+#' @param ... : extra parameters passed to \code{\link[datavolley]{read_dv}} (if \code{dvw} is a provided as a string) and/or to the shiny server and UI functions
 #'
 #' @seealso \code{\link[datavolley]{read_dv}}
 #'
@@ -11,12 +11,13 @@
 ov_shiny_video_sync <- function(dvw, video_file = NULL, launch_browser = TRUE, ...) {
     assert_that(is.flag(launch_browser), !is.na(launch_browser))
     dots <- list(...)
+    dv_read_args <- dots[names(dots) %in% names(formals(datavolley::dv_read))] ## passed to dv_read
+    other_args <- dots[!names(dots) %in% names(formals(datavolley::dv_read))] ## passed to the server and UI
     if (is.string(dvw)) {
         dvw_filename <- dvw
         if (!"skill_evaluation_decode" %in% names(dots)) dots$skill_evaluation_decode <- "guess"
-        rgs <- dots
-        rgs$filename <- dvw
-        dvw <- do.call(datavolley::read_dv, rgs)
+        dv_read_args$filename <- dvw
+        dvw <- do.call(datavolley::read_dv, dv_read_args)
     } else {
         if (!inherits(dvw, "datavolley")) stop("dvw should be a datavolley object or the path to a .dvw file")
         dvw_filename <- dvw$meta$filename
@@ -34,7 +35,7 @@ ov_shiny_video_sync <- function(dvw, video_file = NULL, launch_browser = TRUE, .
         if (!file.exists(dvw$meta$video$file)) stop("specified video file (", dvw$meta$video$file, ") does not exist. Perhaps specify the local path via the video_file parameter?")
     }
     ## finally the shiny app
-    app_data <- list(dvw_filename = dvw_filename, dvw = dvw, dv_read_args = dots)
+    app_data <- c(list(dvw_filename = dvw_filename, dvw = dvw, dv_read_args = dv_read_args), other_args)
     this_app <- list(ui = ov_shiny_video_sync_ui(app_data = app_data), server = ov_shiny_video_sync_server(app_data = app_data))
     shiny::runApp(this_app, display.mode = "normal", launch.browser = launch_browser)
 }
