2a36aa981f5b5735c9b9ac57d98c710e6b4a825c
Ben Raymond <ben@theraymonds.org>
2020-04-15 14:32:05 +1000

    fixes to ovscout.R startup script, add lighttpd path if avail

--- standalone/win/ovscout.R
+++ standalone/win/ovscout.R
@@ -7,41 +7,42 @@ rgs <- commandArgs(trailingOnly = TRUE)
 
 if (length(rgs) < 1) stop('incorrect usage')
 
+options(warn = 1) ## show warnings immediately
+
 mypath <- gsub("^\"+", "", gsub("\"+$", "", rgs[1]))
 ## force R to use local library path
-Rlibpath <- file.path(mypath,"lib")
+i386 <- identical(version$arch, "i386") ## 32-bit windows
+Rlibpath <- file.path(mypath, paste0("lib", if (i386) "i386"))
 if (!dir.exists(Rlibpath)) dir.create(Rlibpath)
 .libPaths(c(Rlibpath, .libPaths()))
 
 ## check that we have dependencies installed
 optsave <- getOption("repos")
 options(repos = c(CRAN = "https://cran.rstudio.com/"))
-online <- tryCatch(suppressWarnings(!is.null(nsl("rstudio.com"))), error = function(e) FALSE)
-online <- FALSE
 
 ## dependencies required before installing ovscout, with optional minimum version number
-depsl <- list(remotes = NA)
+depsl <- list(remotes = NA, fs = NA)
 for (pkg in names(depsl)) {
     if (!requireNamespace(pkg, quietly = TRUE) || (!is.na(depsl[[pkg]]) && packageVersion(pkg) < depsl[[pkg]])) {
-        if (online) {
+        tryCatch({
             cat(sprintf("Installing package: %s\n", pkg))
             install.packages(pkg)
-        } else {
-            warning("Missing a dependency but you appear to be offline so can't install it: things may not work")
-        }
+        }, error = function(e) {
+            stop("Could not install the ", dpkg, " package. The error message was: ", conditionMessage(e))
+        })
     }
 }
 
 ## we will always attempt to install these, so that they are always updated
 github_deps <- c("openvolley/ovscout")
 for (pkg in github_deps) {
-    if (online) {
+    tryCatch({
         remotes::install_github(pkg, quiet = TRUE)
-    } else {
+    }, error = function(e) {
         if (!requireNamespace(basename(pkg), quietly = TRUE)) {
-            stop("Missing the ", pkg ," package but you appear to be offline so can't install it.")
+            stop("Could not install the ", dpkg, " package. The error message was: ", conditionMessage(e))
         }
-    }
+    })
 }
 ## placeholder for optional packages
 ##github_deps <- c("openvolley/peranavolley")
@@ -56,6 +57,16 @@ for (pkg in github_deps) {
 ##}
 options(repos = optsave) ## restore
 
+## add lighttpd folder to path
+have_lighttpd <- tryCatch({
+    chk <- sys::exec_internal("lighttpd", "-version")
+    TRUE
+}, error = function(e) FALSE)
+lhpath <- fs::path_real(fs::path(mypath, "lighttpd"))
+if (!have_lighttpd && dir.exists(lhpath)) {
+    Sys.setenv(path = paste0(lhpath, ";", Sys.getenv("path")))
+}
+
 library(ovscout)
 if (length(rgs) < 2 || is.na(rgs[2]) || !nzchar(rgs[2])) {
     cat("choose dvw file\n")
@@ -66,4 +77,4 @@ if (length(rgs) < 2 || is.na(rgs[2]) || !nzchar(rgs[2])) {
     }
 }
 ## TODO, option to specify video file
-ovscout::ov_shiny_video_sync(dvw = dvw, video_file = NULL, launch_browser = TRUE)
+ov_shiny_video_sync(dvw = dvw, video_file = NULL, launch_browser = TRUE)
