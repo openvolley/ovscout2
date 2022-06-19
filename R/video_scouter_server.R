51066f8f09b3d1a0e40a54acd1295497bc3c98e8
Ben Raymond <ben@theraymonds.org>
2022-06-19 10:35:49 +1000

    fix review pane

--- R/video_scouter_server.R
+++ R/video_scouter_server.R
@@ -1840,17 +1840,8 @@ ov_scouter_server <- function(app_data) {
         show_review_pane <- function() {
             ## use the current video time from the main video
             ## construct the playlist js by hand, because we need to inject the current video time
-            mytype <- "local"
-            if (is_youtube_url(app_data$video_src)) {
-                mysrc <- youtube_url_to_id(app_data$video_src)
-                mytype <- "youtube"
-            } else if (is_url(app_data$video_src)) {
-                mysrc <- app_data$video_src
-            } else {
-                mysrc <- file.path(app_data$video_server_base_url, basename(app_data$video_src))
-            }
-            temp <- paste0("var start_t=vidplayer.currentTime()-2; revpl.set_playlist_and_play([{'video_src':'", mysrc, "','start_time':start_t,'duration':4,'type':'", mytype, "'}], 'review_player', '", mytype, "', true); revpl.set_playback_rate(1.4);")
-            dojs(temp)
+            revsrc <- get_src_type(if (current_video_src() == 1L) app_data$video_src else app_data$video_src2)
+            dojs(paste0("var start_t=vidplayer.currentTime()-2; revpl.set_playlist_and_play([{'video_src':'", revsrc$src, "','start_time':start_t,'duration':4,'type':'", revsrc$type, "'}], 'review_player', '", revsrc$type, "', true); revpl.set_playback_rate(1.4);"))
             js_show2("review_pane")
         }
         hide_review_pane <- function() {
