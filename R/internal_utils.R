6c0991e2785afe129f7343086c5f492fe6e6fba8
Ben Raymond <ben@theraymonds.org>
2022-07-05 20:00:23 +1000

    add lighttpd helper

--- R/internal_utils.R
+++ R/internal_utils.R
@@ -113,3 +113,25 @@ youtube_url_to_id <- function(z) {
     }
 }
 
+ovscout2_app_dir <- function() rappdirs::user_data_dir(appname = "ovscout2")
+
+## adapted from http://conjugateprior.org/2015/06/identifying-the-os-from-r/
+get_os <- function() {
+    if (.Platform$OS.type == "windows") return("windows")
+    sysinf <- Sys.info()
+    if (!is.null(sysinf)){
+        os <- sysinf["sysname"]
+        if (tolower(os) == "darwin")
+            os <- "osx"
+    } else {
+        os <- .Platform$OS.type
+        if (grepl("^darwin", R.version$os, ignore.case = TRUE))
+            os <- "osx"
+        if (grepl("linux-gnu", R.version$os, ignore.case = TRUE))
+            os <- "linux"
+    }
+    os <- tolower(os)
+    if (!os %in% c("windows", "linux", "unix", "osx"))
+        stop("unknown operating system: ", os)
+    os
+}
