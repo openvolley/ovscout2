197cd9b6b05c5f16f4b338994866a7b977f795ae
Ben Raymond <ben@theraymonds.org>
2023-06-23 17:50:37 +1000

    prior attack player func from attack code table

--- R/prior_player_skill.R
+++ R/prior_player_skill.R
@@ -155,3 +155,50 @@ player_responsibility_fn <- function(system = "SHM3", skill, setter_position, zo
     }
     ret
 }
+
+attack_player_prior_by_code <- function(system = "SHM3", setter_position, set_type, attacker_position, home_visiting, serving) {
+    system <- match.arg(system, c("SHM3"))
+    if (system == "SHM3") {
+        ## use set_type (F, C, B, P, S, -) to figure the attack player pos
+        ## also using attacker_position to distinguish front/back row combo codes. If the setter is front row (opposite is back row) and the combo code is for a front-row backset, assign that to the front-row middle, and similarly for a back-row backset when setter is back row
+        ret <- case_when(set_type == "S" ~ setter_position,
+                         setter_position == 1 ~ case_when(isTRUE(serving) ~ case_when(set_type == "B" & attacker_position %in% 2:4 ~ 4L,
+                                                                                      set_type == "B" ~ 3L,
+                                                                                      set_type == "C" ~ 3L,
+                                                                                      set_type == "F" ~ 2L,
+                                                                                      set_type == "P" ~ 5L),
+                                                          TRUE ~ case_when(set_type == "B" & attacker_position %in% 2:4 ~ 2L,
+                                                                           set_type == "B" ~ 3L,
+                                                                           set_type == "C" ~ 3L,
+                                                                           set_type == "F" ~ 4L,
+                                                                           set_type == "P" ~ 5L)),
+                         setter_position == 6 ~ case_when(set_type == "B" & attacker_position %in% 2:4 ~ 3L,
+                                                          set_type == "B" ~ 2L,
+                                                          set_type == "C" ~ 2L,
+                                                          set_type == "F" ~ 4L,
+                                                          set_type == "P" ~ 1L),
+                         setter_position == 5 ~ case_when(set_type == "B" & attacker_position %in% 2:4 ~ 2L,
+                                                          set_type == "B" ~ 4L,
+                                                          set_type == "C" ~ 4L,
+                                                          set_type == "F" ~ 3L,
+                                                          set_type == "P" ~ 6L),
+                         setter_position == 2 ~ case_when(set_type == "C" ~ 4L,
+                                                          set_type == "F" ~ 3L,
+                                                          set_type == "P" ~ 6L,
+                                                          set_type == "B" & attacker_position %in% 2:4 ~ 4L,
+                                                          set_type == "B" ~ 5L),
+                         setter_position == 3 ~ case_when(set_type == "C" ~ 2L,
+                                                          set_type == "F" ~ 4L,
+                                                          set_type == "P" ~ 1L,
+                                                          set_type == "B" & attacker_position %in% 2:4 ~ 2L,
+                                                          set_type == "B" ~ 6L),
+                         setter_position == 4 ~ case_when(set_type == "C" ~ 3L,
+                                                          set_type == "F" ~ 2L,
+                                                          set_type == "P" ~ 5L,
+                                                          set_type == "B" & attacker_position %in% 2:4 ~ 3L,
+                                                          set_type == "B" ~ 1L))
+        ifelse(!is.na(ret), paste0(home_visiting, "_p", ret), NA_character_)
+    } else {
+        stop("unrecognized system: ", system)
+    }
+}
