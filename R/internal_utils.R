b92cc280f5bf216ee70bd6f81a7c96a8173a738c
Ben Raymond <ben@untan.gl>
2024-03-30 18:53:52 +1100

    add %neq% function

--- R/internal_utils.R
+++ R/internal_utils.R
@@ -1,4 +1,5 @@
 `%eq%` <- function (x, y) x == y & !is.na(x) & !is.na(y)
+`%neq%` <- function (x, y) x != y & !is.na(x) & !is.na(y)
 
 is_nnn <- function(z) length(z) < 1 || (length(z) == 1 && (is.na(z) || (z %eq% "none")))
 nn_or <- function(z, or = "") if (is.null(z)) or else z
